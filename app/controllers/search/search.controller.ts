ff1ac9a60e7cedc29c539f4cd4861496ccb647b5
MartynasP <39139245+RealFisherman@users.noreply.github.com>
2018-08-03 12:06:58 +0300

    Added more helpers.

--- app/controllers/search/search.controller.ts
+++ app/controllers/search/search.controller.ts
@@ -1,7 +1,6 @@
 import { Router, Request, Response } from "express";
 
-import { searchForRecipesByTitle } from "./search.controller.helpers";
-import { getDataFromDatabase } from "../../helpers";
+import { searchForRecipesByTitle, getOnePageOfRecipes } from "./search.controller.helpers";
 import { SearchTypes } from "../../models/SearchTypes";
 
 const router: Router = Router();
@@ -11,10 +10,10 @@ router.get("/", (req: Request, res: Response) => {
     const startingRecipe = page * 10 - 10;
     const recipesToShow = 10;
 
-    if (q == null || q === "") {
-        res.send(getDataFromDatabase().splice(startingRecipe, recipesToShow));
+    if (page == null) {
+        res.send(searchForRecipesByTitle());
     } else {
-        res.send(searchForRecipesByTitle(q, page, startingRecipe, recipesToShow));
+        res.send(getOnePageOfRecipes(startingRecipe, recipesToShow, q));
     }
 });
 
